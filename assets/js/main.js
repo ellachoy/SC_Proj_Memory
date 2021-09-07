8620f1d18a2d7e94a854f8ec616da28d282b7199
Ella Choy <egal21de@gmail.com>
2021-09-07 18:05:14 +0200

    zurück

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,4 @@
-console.log('Memorygame')
+console.log('Memorygame');
 
 
-const container=document.querySelector('.container');
-
-const files=[
-    '../img/aquaman.jpg',
-    '../img/batman.jpg',
-    '../img/captain-america.jpg',
-    '../img/fantastic-four.jpg',
-    '../img/flash.jpg',
-    '../img/green-arrow.jpg',
-    '../img/green-lantern.jpg',
-    '../img/ironman.jpg',
-    '../img/spiderman.jpg',
-    '../img/superman.jpg',
-    '../img/the-avengers.jpg'
-  ];
-img.src=files[0];
-  img.forEach(elt=>{
-    let img=document.createElement(img)
-    container.appendChild(img);
-    console.log(elt[1]);
-    console.log(elt);
-});
- const imgs= new Array();
-for (let i=0; i<files.length; i++){
-    // console.log(files[i]);
-    imgs[i]=new Image();
-    imgs[i].src=files[i];
  
-}
