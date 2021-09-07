6480c4fbdafcfb48bb977003226838dc6a4e6bb4
Ella Choy <egal21de@gmail.com>
2021-09-07 15:06:15 +0200

    retry

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,32 @@
 console.log('Memorygame')
 
 
+const container=document.querySelector('.container');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// first try
-// const container=document.querySelector('.container');
-
-// const files=[
-//     '../img/aquaman.jpg',
-//     '../img/batman.jpg',
-//     '../img/captain-america.jpg',
-//     '../img/fantastic-four.jpg',
-//     '../img/flash.jpg',
-//     '../img/green-arrow.jpg',
-//     '../img/green-lantern.jpg',
-//     '../img/ironman.jpg',
-//     '../img/spiderman.jpg',
-//     '../img/superman.jpg',
-//     '../img/the-avengers.jpg'
-//   ];
-// img.src=files[0];
-//   img.forEach(elt=>{
-//     let img=document.createElement(img)
-//     container.appendChild(img);
-//     console.log(elt[1]);
-//     console.log(elt);
-// });
-//  const imgs= new Array();
-// for (let i=0; i<files.length; i++){
-//     // console.log(files[i]);
-//     imgs[i]=new Image();
-//     imgs[i].src=files[i];
+const files=[
+    '../img/aquaman.jpg',
+    '../img/batman.jpg',
+    '../img/captain-america.jpg',
+    '../img/fantastic-four.jpg',
+    '../img/flash.jpg',
+    '../img/green-arrow.jpg',
+    '../img/green-lantern.jpg',
+    '../img/ironman.jpg',
+    '../img/spiderman.jpg',
+    '../img/superman.jpg',
+    '../img/the-avengers.jpg'
+  ];
+img.src=files[0];
+  img.forEach(elt=>{
+    let img=document.createElement(img)
+    container.appendChild(img);
+    console.log(elt[1]);
+    console.log(elt);
+});
+ const imgs= new Array();
+for (let i=0; i<files.length; i++){
+    // console.log(files[i]);
+    imgs[i]=new Image();
+    imgs[i].src=files[i];
  
-// }
+}
